0c149c4415d5eaae33baa9ddf725b272042333e7
orenCPC <oren@cherrypick-consulting.com>
2022-03-02 11:49:31 +0200

    Added a table view controller, with the features of add and delete Image Galleries

---
+++ Exercise5/ImageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImageViewController.swift
+//  Exercise5
+//
+//  Created by Oren Dinur on 02/03/2022.
+//
+
+import UIKit
+
+
+////
+///Stopped at lecture 9 44:00 (It's the lower view on storyboard)
+///
+
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
